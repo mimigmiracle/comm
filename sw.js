1aac904b161890d73935096bddfe4da4e5b85c8b
mimigmiracle <marydiego870@gmail.com>
2026-09-13 15:04:43 +0100

    Simplify fetch event handling in service worker

--- sw.js
+++ sw.js
@@ -1,11 +1,3 @@
-self.addEventListener('install', (e) => {
-  self.skipWaiting();
-});
-
-self.addEventListener('activate', (e) => {
-  return self.clients.claim();
-});
-
-self.addEventListener('fetch', (e) => {
-  // Serves web assets online
-});
+self.addEventListener('fetch', (event) => {
+       event.respondWith(fetch(event.request));
+   });
